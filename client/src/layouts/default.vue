497955377ae7831fafe6a5b173afb06e5e4a2c79
alsey89 <101123930+alsey89@users.noreply.github.com>
2024-06-01 14:32:54 +0800

    fix: client: sidebar animation conflict

--- client/src/layouts/default.vue
+++ client/src/layouts/default.vue
@@ -1,16 +1,15 @@
-<!-- MainComponent.vue -->
 <template>
     <div class="w-full h-screen flex bg-background">
         <!-- button to toggle sidebar -->
-        <Icon v-if="screenWidth < 768 && !showSidebar" @click="toggleSidebar" icon="material-symbols:menu-rounded"
+        <Icon v-if="isMobile && !showSidebar" @click="toggleSidebar" icon="material-symbols:menu-rounded"
             class="fixed top-3 left-3 w-6 h-6 text-primary z-10" />
         <!-- overlay to hide sidebar -->
-        <div v-if="showSidebar && screenWidth < 768" @click="toggleSidebar"
-            class="fixed inset-0 bg-black bg-opacity-40 z-40"></div>
+        <div v-if="showSidebar && isMobile" @click="toggleSidebar" class="fixed inset-0 bg-black bg-opacity-40 z-40">
+        </div>
         <!-- sidebar -->
-        <Sidebar :showSidebar="showSidebar" :screenWidth="screenWidth" />
+        <Sidebar :showSidebar="showSidebar" :isMobile="isMobile" />
         <!-- main content -->
-        <div :class="{ 'ml-[200px]': showSidebar && screenWidth >= 768, 'ml-0': !showSidebar || screenWidth < 768 }"
+        <div :class="{ 'ml-[200px]': showSidebar && !isMobile, 'ml-0': !showSidebar || isMobile }"
             class="w-full h-full overflow-y-scroll">
             <router-view></router-view>
         </div>
@@ -23,18 +22,16 @@ import { Sidebar } from "@/components/app";
 import { Icon } from '@iconify/vue';
 
 const showSidebar = ref(window.innerWidth >= 768);
-const screenWidth = ref(window.innerWidth);
+const isMobile = ref(window.innerWidth < 768);
+
 const toggleSidebar = () => {
     showSidebar.value = !showSidebar.value;
 };
 
 const handleResize = () => {
-    screenWidth.value = window.innerWidth;
-    if (screenWidth.value >= 768) {
-        showSidebar.value = true;
-    } else {
-        showSidebar.value = false;
-    }
+    const width = window.innerWidth;
+    isMobile.value = width < 768;
+    showSidebar.value = width >= 768;
 };
 
 onMounted(() => {
